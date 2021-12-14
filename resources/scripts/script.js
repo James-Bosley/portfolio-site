2763cdb8f7c411c72fcfac29aeb0122beabe75a6
James Bosley <james_bosley@hotmail.co.uk>
2021-12-14 15:08:34 +0000

    Project Added

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -12,5 +12,6 @@ let project1 = document.getElementById('1-title');
 let project2 = document.getElementById('2-title');
 let project3 = document.getElementById('3-title');
 let project4 = document.getElementById('4-title');
+let project5 = document.getElementById('5-title');
 
-[project1,project2,project3,project4].forEach(x => x.addEventListener('click', exposeDetail));
+[project1,project2,project3,project4,project5].forEach(x => x.addEventListener('click', exposeDetail));
